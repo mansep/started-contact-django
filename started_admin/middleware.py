e6716bc41e050fad999423b7026d56e3b00fa0e6
Manuel Sepúlveda <mansep1991@gmail.com>
2019-01-21 00:11:02 -0300

    skip static and meddia in middleware

--- started_admin/middleware.py
+++ started_admin/middleware.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 
+import pdb
+
 class ProfileMiddleware:
 
 	def __init__(self, get_response):
@@ -8,15 +10,16 @@ class ProfileMiddleware:
 
 
 	def __call__(self, request):
-		
-		if request.path not in [reverse('profile_update'), reverse('logout'), reverse('login')]:
-			try:
-				profile = request.user.profile
-			except Exception as e:
-				return redirect('profile_update')
-			
-			if not profile:
-				return redirect('profile_update')
+		if request.user.is_authenticated:
+			if request.path not in [reverse('profile_update'), reverse('logout'), reverse('login')]:
+				if request.path[1:6] not in ('stati', 'media')
+					try:
+						profile = request.user.profile
+					except Exception as e:
+						return redirect('profile_update')
+					
+					if not profile:
+						return redirect('profile_update')
 				
 		response = self.get_response(request)
 		return response
